3e2bd9388e1cac38d755483b9aef8c0c5e4397c4
FranGalluccio <50331388+FranGalluccio@users.noreply.github.com>
2021-06-24 16:01:30 +0200

    Update Avvio.java

--- MiaPrimaCondivisione/src/Avvio.java
+++ MiaPrimaCondivisione/src/Avvio.java
@@ -2,7 +2,7 @@
 public class Avvio {
 
 	//Heyla!!! Saluti da Mirko!!!
-	
+	// Ciaoooooooooooooooooo!!! :D:D:D:D
 	public static void main(String[] args) {
 		System.out.println("ciao a tutti!!");
 
